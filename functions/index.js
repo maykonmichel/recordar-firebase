68e8c74f4a4d81c672bdc58683aa1ad3f237b707
maykonmichel <maykonmichelpalma@gmail.com>
2020-06-09 18:26:45 -0300

    Added create payment

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,27 @@
 const functions = require('firebase-functions');
+const mercadopago = require('mercadopago');
 
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+mercadopago.configure({
+  access_token: 'APP_USR-6717864324430689-051816-20baf9cd6ecd11e2549c93c614eb7b1d-570070788'
+});
+
+exports.createPreference = functions.https.onRequest(async (request, response) => {
+  try {
+    const { body: { init_point } } = await mercadopago.preferences.create({
+      items: [
+        {
+          title: 'Recordar app',
+          unit_price: 29.9,
+          quantity: 1,
+        }
+      ],
+      back_urls: {
+        success: 'success',
+      },
+      binary_mode: true
+    });
+    return response.redirect(`${init_point}`);
+  } catch ({ message }) {
+    return response.send(message);
+  }
+});
